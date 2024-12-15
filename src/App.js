7b6c5f2be30b445025b4a1437d9707fb11d9c4fd
koyndevighnesh77 <koyandevighnesh77@gmail.com>
2024-12-15 19:29:09 +0530

    Landing page done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import './home_styles.css'; // Importing the CSS file for Home component
+import Home from './components/Home'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Home /> {/* Rendering the Home component */}
+    </div>
+  );
+}
+
+export default App;
